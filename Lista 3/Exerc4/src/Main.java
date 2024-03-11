4b671b30b27e950948b1093dcaf35b7bcb45e03f
ZKNs1 <msaori40@gmail.com>
2024-03-11 14:59:10 -0300

    finalizacao

--- Lista 3/Exerc4/src/Main.java
+++ Lista 3/Exerc4/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Tabela de Conversão de Celsius para Fahrenheit:");
+        System.out.println("Celsius\tFahrenheit");
 
-        for(int i=1; i < 90; i++) {
-            if (i % 4 == 0) {
-                System.out.println("Os números divisíveis por 4 e menor que 200 é: " + i);
-            }
+        for (int celsius = -80; celsius <= 80; celsius += 10) {
+            double fahrenheit = celsiusToFahrenheit(celsius);
+            System.out.println(celsius + "\t" + fahrenheit);
         }
+    }
+
+    public static double celsiusToFahrenheit(int celsius) {
+        return (9.0 / 5.0) * celsius + 32;
     }
 }
